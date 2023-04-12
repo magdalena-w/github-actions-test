236b1fa736686c96f29e4f4b027ff8bb3807bdaa
magdalena-w <walesa.magdalena@gmail.com>
2023-04-12 13:16:01 +0200

    Adding error to test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestAdd(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1, 1), 2)
         self.assertEqual(add(10, 0), 10)
-        self.assertEqual(add(-2, 8), 6)
+        self.assertEqual(add(-2, 8), 4)
 
 if __name__ == '__main__':
     unittest.main()
