a00419551472c6b459a1279fb10b08822dc8a25f
magdalena-w <walesa.magdalena@gmail.com>
2023-04-12 12:33:45 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from add import add
+
+class TestAdd(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 1), 2)
+        self.assertEqual(add(10, 0), 10)
+        self.assertEqual(add(-2, 8), 6)
+
+if __name__ == '__main__':
+    unittest.main()
